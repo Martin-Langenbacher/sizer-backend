0105280bee66e372693e8b3a010766ba22fccb36
Martin Langenbacher <martin.langenbacher@telekom.de>
2021-01-29 17:24:06 +0100

    first commit

---
+++ src/main/java/de/langenbachersolar/sizer/sizerbackend/SizerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.langenbachersolar.sizer.sizerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SizerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SizerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/langenbachersolar/sizer/sizerbackend/SizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.langenbachersolar.sizer.sizerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SizerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
